ce378c11f86134f8593f80018f239a901159ac4d
eduardo souza <lkledu@gmail.com>
2025-11-04 16:41:18 -0300

    creating squad tab view

---
+++ lib/widgets/squadTab.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class SquadTab extends StatefulWidget {
+  const SquadTab({super.key});
+
+  @override
+  State<StatefulWidget> createState() => SquadTabState();
+}
+
+class SquadTabState extends State<SquadTab>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
