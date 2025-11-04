b343ab2cd9e3ffd08da0b26b53df744ba102279b
eduardo souza <lkledu@gmail.com>
2025-11-04 16:42:56 -0300

    creating user tab

---
+++ lib/widgets/userTab.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class UserTab extends StatefulWidget {
+  const UserTab({super.key});
+
+  @override
+  State<StatefulWidget> createState() => UserTabState();
+}
+
+class UserTabState extends State<UserTab>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
