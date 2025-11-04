0268f12359db406300d3131ce9da70d436941db5
eduardo souza <lkledu@gmail.com>
2025-11-04 20:18:34 -0300

    adding Criar Lançamento modal

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,13 +1,46 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pd_hour_control_app/widgets/criarLancamentoModal.dart';
 
 class Header extends StatelessWidget{
+  const Header({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text('PD Hours'),
-        ElevatedButton(onPressed: (){}, child: Text('Lançar horas'))
+        Text(
+          'PD Hours',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 43.0
+          ),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    scrollable: true,
+                    title: Text('Criar lançamento'),
+                    content: CriarLancamentoModal(),
+                  );
+                }
+              );
+            },
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(Color(0xFF4263EB)),
+              shape: WidgetStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+            ),
+            child: Text('Lançar horas', style: TextStyle(color: Colors.white),),
+          ),
+        )
       ],
     );
   }
