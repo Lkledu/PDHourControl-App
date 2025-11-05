ac94ebff96825aca7727aea11c6f704840cda50b
eduardo souza <lkledu@gmail.com>
2025-11-05 19:00:07 -0300

    adding user table list

--- lib/widgets/userTab.dart
+++ lib/widgets/userTab.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pd_hour_control_app/controllers/pdHourControlController.dart';
+import 'package:pd_hour_control_app/widgets/alertBtn.dart';
+import 'package:pd_hour_control_app/widgets/criarUsuarioModal.dart';
+import 'package:pd_hour_control_app/widgets/customButton.dart';
 
 class UserTab extends StatefulWidget {
   const UserTab({super.key});
@@ -8,9 +12,36 @@ class UserTab extends StatefulWidget {
 }
 
 class UserTabState extends State<UserTab>{
+  final PdHourControlController _controller = PdHourControlController();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Table(
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            children: [
+              TableRow(children: [Text('Nome'), Text('Horas'), Text('SquadId')]),
+              ..._controller.getEmployees().map(
+                (user) {
+                  return TableRow(
+                      children: [
+                        Text(user.name),
+                        Text(user.estimatedHours.toString()),
+                        Text(user.squadId.toString())
+                      ]
+                  );
+                }
+              ),
+            ]
+        ),
+        Center(
+            child: AlertBtn(
+                text: 'Criar Usuário',
+                content: CriarUsuarioModal()
+            )
+        )
+      ],
+    );
   }
 }
