fe6599e65a7feadb201a71cc8c8cae8ee9b6fd08
eduardo souza <lkledu@gmail.com>
2025-11-05 18:59:50 -0300

    adding squad table list

--- lib/widgets/squadTab.dart
+++ lib/widgets/squadTab.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pd_hour_control_app/controllers/pdHourControlController.dart';
+import 'package:pd_hour_control_app/widgets/alertBtn.dart';
+import 'package:pd_hour_control_app/widgets/criarSquadModal.dart';
 
 class SquadTab extends StatefulWidget {
   const SquadTab({super.key});
@@ -8,9 +11,35 @@ class SquadTab extends StatefulWidget {
 }
 
 class SquadTabState extends State<SquadTab>{
+  final PdHourControlController _controller = PdHourControlController();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Table(
+          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+          children: [
+            TableRow(children: [Text('Id'), Text('Nome')]),
+            ..._controller.getSquads().map(
+                (squad) {
+                  return TableRow(
+                    children: [
+                      Text(squad.id.toString()),
+                      Text(squad.name)
+                    ]
+                  );
+                }
+            ),
+          ]
+        ),
+        Center(
+          child: AlertBtn(
+            text: 'Criar Squad',
+            content: CriarSquadModal()
+          )
+        )
+      ]
+    );
   }
 }
