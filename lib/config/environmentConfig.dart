0f700884dd7f16665562971a7e1dbce9227994ad
eduardo souza <lkledu@gmail.com>
2025-11-06 02:44:01 -0300

    adding post endpoints

--- lib/config/environmentConfig.dart
+++ lib/config/environmentConfig.dart
@@ -5,4 +5,7 @@ class EnvironmentConfig {
   static String get fileName => kReleaseMode ? ".env.prod" : ".env.dev";
 
   static String get urlBase => dotenv.env['URL_BASE'] ?? 'Not_found_URL_BASE';
+  static String get urlPostEmployees => dotenv.env['URL_POST_EMPLOYEES'] ?? 'Not_found_URL_POST_EMPLOYEES';
+  static String get urlPostReports => dotenv.env['URL_POST_REPORTS'] ?? 'Not_found_URL_POST_REPORTS';
+  static String get urlPostSquads => dotenv.env['URL_POST_SQUADS'] ?? 'Not_found_URL_POST_SQUADS';
 }
