38dd8981bcb9c308d267eb90239d6398f946e37d
eduardo souza <lkledu@gmail.com>
2025-11-06 03:04:35 -0300

    adding get endpoints

--- lib/config/environmentConfig.dart
+++ lib/config/environmentConfig.dart
@@ -5,7 +5,9 @@ class EnvironmentConfig {
   static String get fileName => kReleaseMode ? ".env.prod" : ".env.dev";
 
   static String get urlBase => dotenv.env['URL_BASE'] ?? 'Not_found_URL_BASE';
-  static String get urlPostEmployees => dotenv.env['URL_POST_EMPLOYEES'] ?? 'Not_found_URL_POST_EMPLOYEES';
-  static String get urlPostReports => dotenv.env['URL_POST_REPORTS'] ?? 'Not_found_URL_POST_REPORTS';
-  static String get urlPostSquads => dotenv.env['URL_POST_SQUADS'] ?? 'Not_found_URL_POST_SQUADS';
+  static String get urlPostEmployees => dotenv.env['URL_POST_EMPLOYEE'] ?? 'Not_found_URL_POST_EMPLOYEE';
+  static String get urlPostReports => dotenv.env['URL_POST_REPORT'] ?? 'Not_found_URL_POST_REPORT';
+  static String get urlPostSquads => dotenv.env['URL_POST_SQUAD'] ?? 'Not_found_URL_POST_SQUAD';
+  static String get urlGetEmployees => dotenv.env['URL_GET_EMPLOYEES'] ?? 'Not_found_URL_GET_EMPLOYEES';
+  static String get urlGetSquads => dotenv.env['URL_GET_SQUADS'] ?? 'Not_found_URL_GET_SQUADS';
 }
--- lib/repositories/pdHourControlRepository.dart
+++ lib/repositories/pdHourControlRepository.dart
@@ -1,17 +1,69 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:pd_hour_control_app/config/environmentConfig.dart';
 import 'package:pd_hour_control_app/repositories/ReportEntity.dart';
 import 'package:pd_hour_control_app/repositories/employeeEntity.dart';
 import 'package:pd_hour_control_app/repositories/squadEntity.dart';
 
 class PdHourControlRepository {
-  Future<List<EmployeeEntity>> getEmployees (){
-    return Future.error(Error);
+  Future<int> postEmployees (EmployeeEntity employee) async {
+    EmployeeEntity employeeEntity = employee;
+    final url = Uri.parse(EnvironmentConfig.urlBase+EnvironmentConfig.urlPostEmployees);
+
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type':'application/json',
+        'Accept': 'application/json',
+      },
+      body: jsonEncode(employeeEntity.toJson())
+    );
+
+    if(response.statusCode == 200){
+      return 1;
+    }
+
+    return -1;
   }
 
-  Future<List<SquadEntity>> getSquads() {
-    return Future.error(Error);
+  Future<int> postSquads(SquadEntity squad) async {
+    SquadEntity squadEntity = squad;
+    final url = Uri.parse(EnvironmentConfig.urlBase+EnvironmentConfig.urlPostEmployees);
+
+    final response = await http.post(
+        url,
+        headers: {
+          'Content-Type':'application/json',
+          'Accept': 'application/json',
+        },
+        body: jsonEncode(squadEntity.toJson())
+    );
+
+    if(response.statusCode == 200){
+      return 1;
+    }
+
+    return -1;
   }
 
-  Future<List<ReportEntity>> getReports() {
-    return Future.error(Error);
+  Future<int> postReports(ReportEntity report) async{
+    ReportEntity reportEntity = report;
+    final url = Uri.parse(EnvironmentConfig.urlBase+EnvironmentConfig.urlPostEmployees);
+
+    final response = await http.post(
+        url,
+        headers: {
+          'Content-Type':'application/json',
+          'Accept': 'application/json',
+        },
+        body: jsonEncode(reportEntity.toJson())
+    );
+
+    if(response.statusCode == 200){
+      return 1;
+    }
+
+    return -1;
   }
 }
