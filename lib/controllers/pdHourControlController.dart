512809bd2d745a789c2779009289c521af6898b1
eduardo souza <lkledu@gmail.com>
2025-11-06 03:06:26 -0300

    adding create squads, create employees, get squads, get employees to controller and service

--- lib/controllers/pdHourControlController.dart
+++ lib/controllers/pdHourControlController.dart
@@ -4,17 +4,26 @@ import 'package:pd_hour_control_app/services/pdHourControlService.dart';
 
 class PdHourControlController {
   PdHourControlService _service = PdHourControlService();
-  List<SquadEntity> getSquads() {
-    /*List<SquadEntity> list = [];
-    _service.getSquads().then((e){list = e;});
-    return list;*/
-    return [];
-  }
 
   List<EmployeeEntity> getEmployees() {
-    /*List<EmployeeEntity> list = [];
+    List<EmployeeEntity> list = [];
     _service.getEmployees().then((e){list = e;});
-    return list;*/
-    return [];
+    return list;
+  }
+
+  List<SquadEntity> getSquads() {
+    List<SquadEntity> list = [];
+    _service.getSquads().then((e){list = e;});
+    return list;
+  }
+
+  void createSquads(String name) {
+    SquadEntity squad = SquadEntity(id: 0, name: name);
+    _service.createSquad(squad);
+  }
+
+  void createEmployees(String name, int estimatedHours, int squadId) {
+    EmployeeEntity employee = EmployeeEntity(id: 0, name: name, estimatedHours: estimatedHours, squadId: squadId);
+    _service.createEmployee(employee);
   }
 }
--- lib/services/pdHourControlService.dart
+++ lib/services/pdHourControlService.dart
@@ -3,13 +3,21 @@ import 'package:pd_hour_control_app/repositories/pdHourControlRepository.dart';
 import 'package:pd_hour_control_app/repositories/squadEntity.dart';
 
 class PdHourControlService {
-  PdHourControlRepository _repository = PdHourControlRepository();
+  final PdHourControlRepository _repository = PdHourControlRepository();
 
-  Future<List<SquadEntity>> getSquads(){
-    return _repository.getSquads();
+  Future<int> createSquad(SquadEntity squad) async {
+    return _repository.postSquad(squad);
   }
 
-  Future<List<EmployeeEntity>> getEmployees() {
+  Future<int> createEmployee(EmployeeEntity employee) async {
+    return _repository.postEmployee(employee);
+  }
+
+  Future<List<EmployeeEntity>> getEmployees() async {
     return _repository.getEmployees();
+  }
+
+  Future<List<SquadEntity>> getSquads() async {
+    return _repository.getSquads();
   }
 }
